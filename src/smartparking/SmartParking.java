27c30a59f76a7138dcc4fed4b852910f7ab174f8
roonn <roonn@PC-ROON_NY>
2026-03-07 13:34:44 -0500

    Primer commit Smart Parking System

---
+++ src/smartparking/SmartParking.java
@@ -0,0 +1,17 @@
+
+package smartparking;
+
+/**
+ *
+ * @author roonn
+ */
+public class SmartParking {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Kevin Gay");
+    }
+    
+}
